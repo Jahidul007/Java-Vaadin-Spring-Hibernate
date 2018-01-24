e9541952e9e84482e6649d993e5d10069dbf18c1
Jahidul Islam <jahidul479@gmail.com>
2018-01-25 00:47:38 +0600

    initial commit
    Multiple module configuration with maven

---
+++ 003 Multiple module/student-UI/src/App.java
@@ -0,0 +1,8 @@
+import com.example.util.utils;
+
+public class App {
+   public static void main (String[] args )
+   {
+	   System.out.println(utils.GETTING);
+   }
+}
---
+++ 003 Multiple module/student-util/src/com/example/util/utils.java
@@ -0,0 +1,10 @@
+package com.example.util;
+
+public class utils {
+	private utils()
+	{
+		
+	}
+
+	public static final String GETTING = " Here! Multiple appp";
+}
