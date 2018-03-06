ce6ec4c5768bf8894ded013e149c37acbf609f67
Jahidul Islam <jahidul479@gmail.com>
2018-03-06 12:37:10 +0600

    initial commit

---
+++ 015 Spring bean init & destroy/src/main/java/com/example/maventest/App.java
@@ -0,0 +1,23 @@
+package com.example.maventest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/example/beans/beans.xml");
+    	Patient patient = (Patient) context.getBean("patient");
+    
+        System.out.println(patient);
+
+        //System.out.println( address );
+    	((ClassPathXmlApplicationContext) context ).close();
+    }
+}
