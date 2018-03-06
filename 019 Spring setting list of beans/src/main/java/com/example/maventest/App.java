eecd7078eb542a72fee8e0a16a7d2b1f0bf4aa17
Jahidul Islam <jahidul479@gmail.com>
2018-03-07 04:20:21 +0600

    initial commit

---
+++ 019 Spring setting list of beans/src/main/java/com/example/maventest/App.java
@@ -0,0 +1,22 @@
+package com.example.maventest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/beans/beans.xml");
+		Patient patient = (Patient) context.getBean("patient");
+
+		for (EmergencyContact contact : patient.getEmergencyContact()) {
+			System.out.println(contact);
+		}
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+}
