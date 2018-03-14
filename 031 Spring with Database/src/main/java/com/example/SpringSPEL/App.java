f9ad37efdd33980e388ddfe1343f0ac254e49a42
Jahidul Islam <jahidul479@gmail.com>
2018-03-14 14:44:33 +0600

    update with exception handling

--- 031 Spring with Database/src/main/java/com/example/SpringSPEL/App.java
+++ 031 Spring with Database/src/main/java/com/example/SpringSPEL/App.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 /**
  * Hello world!
@@ -13,12 +14,18 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/beans/beans.xml");
 
-		NoticesDAO dao = (NoticesDAO) context.getBean("dao");
+		NoticesDAO noticeDao = (NoticesDAO) context.getBean("noticeDao");
 
-		List<Notice> notices = dao.getNotices();
+		try {
+			List<Notice> notices = noticeDao.getNotices();
 
-		for (Notice notice : notices) {
-			System.out.println(notice);
+			for (Notice notice : notices) {
+				System.out.println(notice);
+			}
+
+		} catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println(ex.getClass());
 
 		}
 
