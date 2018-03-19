8bd7045b641720584cef757eb4cf1c5a309fb17c
Jahidul Islam <jahidul479@gmail.com>
2018-03-19 22:58:42 +0600

    initial commit

---
+++ 033 Updating (crud) database/src/main/java/com/example/SpringSPEL/App.java
@@ -0,0 +1,47 @@
+package com.example.SpringSPEL;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/beans/beans.xml");
+
+		NoticesDAO noticeDao = (NoticesDAO) context.getBean("noticeDao");
+
+		try {
+			/*delete operation
+			noticeDao.delete(3);*/
+			
+			Notice notice1 = new Notice("parthiv","parthiv@gmail.com","i love to play cricket");
+			noticeDao.create(notice1);
+			
+			List<Notice> notices = noticeDao.getNotices(); 
+			for (Notice notice : notices) {
+				System.out.println(notice);
+			}
+			
+
+		} catch (CannotGetJdbcConnectionException ex1) {
+			System.out.println(ex1.getMessage());
+			System.out.println(ex1.getClass());
+
+		}catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println(ex.getClass());
+
+		}
+		
+
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+}
