878a8307a372923d1740999d09fbf934fedeedf1
Jahidul Islam <jahidul479@gmail.com>
2018-04-03 04:51:49 +0600

    initial commit

---
+++ 039 Spring MVC-Configure with own message/src/com/example/springdemo/mvc/Customer.java
@@ -0,0 +1,74 @@
+package com.example.springdemo.mvc;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Customer {
+
+	private String firstName;
+
+	@NotNull(message = "is required")
+	@Size(min = 3, message = "is required")
+	private String lastName;
+
+	@NotNull(message = "is required")
+	@Min(value = 0, message = "must be greater than or equal to zero")
+	@Max(value = 10, message = "must be less than or equal to ten")
+	private Integer freePasses;
+
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Exactly 5 chars/digits")
+	private String postalCode;
+
+	
+	private String courseCode;
+
+	public Customer() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+ 
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+}
